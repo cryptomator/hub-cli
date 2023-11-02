888923487d944aa5c1040dc9b9aab5d61b3919a8
Armin Schrenk <armin.schrenk@skymatic.de>
2023-11-02 15:54:14 +0100

    add create vault command

--- src/main/java/org/cryptomator/hubcli/CreateVault.java
+++ src/main/java/org/cryptomator/hubcli/CreateVault.java
@@ -1,13 +1,33 @@
 package org.cryptomator.hubcli;
 
 import picocli.CommandLine;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Command;
 
 @Command(name = "create-vault",//
         description = "Create a new vault" )
 class CreateVault implements Runnable {
+
+    @Option(names = { "--vaultId" }, description = "id of the vault")
+    String vaultId;
+    @Option(names = { "--name" }, description = "name of the vault")
+    String name;
+    @Option(names = { "--description" }, description = "description of the vault")
+    String description;
+    @Option(names = { "--outputFile" }, description = "path where to store the returned vault template")
+    String outpoutFile;
+    @Option(names = { "--no-recovery-key" }, description = "suppress output of recovery key")
+    boolean noRecoveryKey;
+
     @Override
     public void run() {
-        System.out.println("This is a stub too!");
+        //generate masterkey, itartions, salt
+
+        if(!noRecoveryKey) {
+            //show recovery key on std.out
+            System.out.println("Recovery key");
+        }
+        //download template and save it to outputFile
+        return;
     }
 }
