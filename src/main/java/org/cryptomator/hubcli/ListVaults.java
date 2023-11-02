fbd08e3b6f92215265e3c8625e4612fc03fd6e08
Armin Schrenk <armin.schrenk@skymatic.de>
2023-11-02 16:37:38 +0100

    add spec draft for listVaults

--- src/main/java/org/cryptomator/hubcli/ListVaults.java
+++ src/main/java/org/cryptomator/hubcli/ListVaults.java
@@ -6,9 +6,22 @@ import picocli.CommandLine;
         description = "List all vaults.")
 class ListVaults implements Runnable {
 
+    //TODO: ideas
+    // --format: [json | csv]
+    // --properties: list of properties to output
+
+    @CommandLine.Option(names = { "--full" }, description = "Print all properties of each vault")
+    boolean fullInfo;
 
     @Override
     public void run() {
+        //http request
+        //parse response
+        if(fullInfo) {
+            //everything
+        } else {
+           //only output name, timestamp, uuid
+        }
         System.out.println("This is a stub");
     }
 }
