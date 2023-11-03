bffcf2805530976c4a758990958ff75f9df68c0e
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2023-11-03 10:49:51 +0100

    login using auth code or client credentials

---
+++ src/main/java/org/cryptomator/hubcli/Common.java
@@ -0,0 +1,62 @@
+package org.cryptomator.hubcli;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import picocli.CommandLine.Option;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+class Common {
+
+	@Option(names = {"--api-base"}, required = true, description = "API base URL of Cryptomator Hub, defaults to $HUB_CLI_API_BASE", defaultValue = "${env:HUB_CLI_API_BASE}")
+	private URI apiBase;
+
+	private Config config;
+
+	public Config getConfig() throws IOException, InterruptedException {
+		if (config == null) {
+			try (var client = HttpClient.newHttpClient()){
+				var uri = getApiBase().resolve("config");
+				var req = HttpRequest.newBuilder().GET().uri(uri).build();
+				var res = client.send(req, HttpResponse.BodyHandlers.ofString());
+				if (res.statusCode() != 200) {
+					throw new IOException("GET " + uri + " resulted in http status code " + res.statusCode());
+				}
+				var json = new ObjectMapper().reader().readTree(res.body());
+				config = new Config(json);
+			}
+		}
+		return config;
+	}
+
+	public URI getApiBase() {
+		// make sure to always end on "/":
+		return URI.create(apiBase.toString() + "/").normalize();
+	}
+
+	record Config(JsonNode config) {
+
+		private static final String AUTH_ENDPOINT_KEY = "keycloakAuthEndpoint";
+		private static final String TOKEN_ENDPOINT_KEY = "keycloakTokenEndpoint";
+		private static final String API_LEVEL_KEY = "apiLevel";
+
+		public URI getAuthEndpoint() {
+			return URI.create(config.get(AUTH_ENDPOINT_KEY).asText());
+		}
+
+		public URI getTokenEndpoint() {
+			return URI.create(config.get(TOKEN_ENDPOINT_KEY).asText());
+		}
+
+		public int getAPILevel() {
+			return config.get(API_LEVEL_KEY).asInt();
+		}
+
+	}
+
+
+}
--- src/main/java/org/cryptomator/hubcli/Login.java
+++ src/main/java/org/cryptomator/hubcli/Login.java
@@ -1,54 +1,91 @@
 package org.cryptomator.hubcli;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.coffeelibs.tinyoauth2client.TinyOAuth2;
 import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Mixin;
 
-import java.net.URI;
 import java.net.http.HttpClient;
-import java.util.Objects;
+import java.net.http.HttpResponse;
+import java.nio.CharBuffer;
+import java.util.Arrays;
 import java.util.concurrent.Callable;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
-import static org.cryptomator.hubcli.HubCli.*;
 
-@CommandLine.Command(name = "login",
-        description = "Login to the hub instance and retrieve an access token.")
-class Login implements Callable<Integer> {
+@Command(name = "login", description = "Login to the hub instance and retrieve an access token.", subcommands = {Login.ClientCredentials.class, Login.AuthorizationCode.class})
+class Login {
 
-    @Override
-    public Integer call() throws Exception {
-        var env = System.getenv();
-        String clientId = env.get(CLIENT_ID_KEY);
-        String clientSecret = env.get(CLIENT_SECRET_KEY);
-        String tokenEndpointUrl = env.get(TOKEN_ENDPOINT_KEY);
-        if (clientId == null || clientSecret == null || tokenEndpointUrl == null) {
-            System.err.println("Required environment variables %s, %s or %s could not be found in environment.".formatted(CLIENT_ID_KEY, CLIENT_SECRET_KEY, TOKEN_ENDPOINT_KEY));
-            return 3;
-        }
+	@Option(names = {"--client-id"}, required = true, description = "Client Id, defaults to $HUB_CLI_CLIENT_ID", defaultValue = "${env:HUB_CLI_CLIENT_ID}")
+	String clientId;
 
-        var authResponse = TinyOAuth2.client(clientId)
-                .withTokenEndpoint(URI.create(tokenEndpointUrl))
-                .clientCredentialsGrant(UTF_8, clientSecret)
-                .authorize(HttpClient.newHttpClient());
+	@Command(name = "client-credentials", description = "Use the Client Credentials Flow.")
+	static class ClientCredentials implements Callable<Integer> {
 
-        var statusCode = authResponse.statusCode();
-        if (statusCode != 200) {
-            System.err.println("""
-            Request was responded with code %d and body:
-            %s
-            """.formatted(statusCode, authResponse.body()));
-            return statusCode;
-        }
+		@Mixin
+		Common common;
 
-        System.out.println(extractAccessToken(authResponse.body()));
-        return 0;
-    }
+		@CommandLine.ParentCommand
+		Login login;
 
-    private String extractAccessToken(String jsonString) {
-        var key = "access_token";
+		@Option(names = {"--client-secret"}, required = true, interactive = true, description = "Client Secret, defaults to $HUB_CLI_CLIENT_SECRET", defaultValue = "${env:HUB_CLI_CLIENT_SECRET}")
+		char[] clientSecret;
 
-        var index= jsonString.indexOf(key)+key.length();
-        return "asd";
-    }
+		@Override
+		public Integer call() throws Exception {
+			var authResponse = TinyOAuth2.client(login.clientId)
+					.withTokenEndpoint(common.getConfig().getTokenEndpoint())
+					.clientCredentialsGrant(UTF_8, CharBuffer.wrap(clientSecret))
+					.authorize(HttpClient.newHttpClient());
+
+			Arrays.fill(clientSecret, ' ');
+
+			return printAccessToken(authResponse);
+		}
+
+	}
+
+	@Command(name = "authorization-code", description = "Use the Authorization Code Flow with PKCE.")
+	static class AuthorizationCode implements Callable<Integer> {
+
+		@CommandLine.ParentCommand
+		Login login;
+
+		@Mixin
+		Common common;
+
+		@Override
+		public Integer call() throws Exception {
+			var authResponse = TinyOAuth2.client(login.clientId)
+					.withTokenEndpoint(common.getConfig().getTokenEndpoint())
+					.authorizationCodeGrant(common.getConfig().getAuthEndpoint())
+					.authorize(HttpClient.newHttpClient(), uri -> {
+						System.out.println("Please login on " + uri);
+					});
+
+			return printAccessToken(authResponse);
+		}
+
+	}
+
+
+	private static int printAccessToken(HttpResponse<String> response) throws JsonProcessingException {
+		var statusCode = response.statusCode();
+		if (statusCode != 200) {
+			System.err.println("""
+					Request was responded with code %d and body:
+					%s
+					""".formatted(statusCode, response.body()));
+			return statusCode;
+		}
+
+		var token = new ObjectMapper().reader().readTree(response.body()).get("access_token").asText();
+		System.out.println(token);
+		return 0;
+	}
+
 }
 
